a85f742d5cc7f14d442eb4288ea7e7d856b43c26
greenwalk <93359072+greenwalk@users.noreply.github.com>
2021-12-30 18:08:31 +0900

    出品機能に関連するバリデーション及びアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :ship_period
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :product, presence: true
+  validates :instruction, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :ship_period_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, presence:true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
