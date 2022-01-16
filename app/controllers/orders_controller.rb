54f156d58374570b5a13ee78ad325cd82cafbb7c
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-01-16 23:05:27 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,47 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create, :pay_item]
+  before_action :redirect_for_sold_out, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+
+  def index
+    @order_residence = OrderResidence.new
+  end
+
+  def create
+    @order_residence = OrderResidence.new(order_params)
+    if @order_residence.valid?
+      pay_item
+      @order_residence.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_residence).permit(:post_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def redirect_for_sold_out
+      redirect_to root_path unless @item.order == nil
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id
+  end
 end
