eb73faaaf6721af0b251ae2f481dd9bc9d6d89f9
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-01-16 17:33:22 +0900

    Formオブジェクトを用いた購入機能のモデル単体テスト用の記述

---
+++ spec/factories/order_residences.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_residence do
+    post_code     {'123-4567'}
+    prefecture_id {2}
+    municipality  {'品川区'}
+    address       {'1-1'}
+    building      {'品川ビルディング'}
+    phone_number  {'0000000000'}
+  end
+end
