b09eb82cb2f5f7329fc0475bfff90818011c9c0a
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-01-16 14:13:12 +0900

    Formオブジェクトの作成

---
+++ app/models/order_residence.rb
@@ -0,0 +1,19 @@
+class OrderResidence
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :post_code, :prefecture, :municipality, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}   
+    validates :municipality 
+    validates :address      
+    validates :phone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}, length: { in: 10..11, message: "is too short"}
+  end
+  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Residence.create(:post_code post_code, :prefecture prefecture, :municipality municipality, :address address, :building building, :phone_number phone_number, :order_id order.id)
+  end
+end
